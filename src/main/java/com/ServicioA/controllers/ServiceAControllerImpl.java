b211cc6bb58a1a52f24b09ca71da8c8e2f75a8f4
juanpditella <jditella@alumnos.exa.unicen.edu.ar>
2023-08-28 17:04:05 -0300

    Configuracion para endpoints correctos y facil ajuste de nuevos heroes

--- src/main/java/com/ServicioA/controllers/ServiceAControllerImpl.java
+++ src/main/java/com/ServicioA/controllers/ServiceAControllerImpl.java
@@ -5,16 +5,20 @@ import com.ServicioA.entities.response_entities.CollaborationResponse;
 import com.ServicioA.exceptions.HeroNotFoundException;
 import com.ServicioA.exceptions.LastTimeNotFoundException;
 import com.ServicioA.services.ServiceAService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/marvel/colaborators")
 public class ServiceAControllerImpl implements ServiceAController {
     private ServiceAService serviceAService;
-
+    @Value("${heroes.endpoints}")
+    private String heroesEndpoints;
     public ServiceAControllerImpl (ServiceAService serviceAService){
         this.serviceAService=serviceAService;
     }
@@ -22,17 +26,29 @@ public class ServiceAControllerImpl implements ServiceAController {
     @GetMapping("/{character}")
     public ResponseEntity<CollaborationResponse> getCollaborators(@PathVariable String character) {
         CollaborationResponse collaborationResponse = new CollaborationResponse();
+        String realName = this.getRealNameForEndpoint(character);
+
         try {
-            collaborationResponse = this.serviceAService.getCollaborators(character);
+            collaborationResponse = this.serviceAService.getCollaborators(realName);
             collaborationResponse.setMessage("El servicio fue ejecutado correctamente.");
         } catch (HeroNotFoundException hnf) {
-            collaborationResponse.setMessage("El heroe "+character+ " no es un heroe presente en nuestra base de datos.");
+            collaborationResponse.setMessage("El valor de la ruta definida no corresponde con ningun heroe cargado en nuestra base de datos.");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(collaborationResponse);
         } catch (LastTimeNotFoundException ltnf) {
             collaborationResponse.setMessage("No existe una sincronizacion de datos valida en nuestra base de datos.");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(collaborationResponse);
         }
         return ResponseEntity.ok(collaborationResponse);
+    }
+
+    private String getRealNameForEndpoint(String endpoint) {
+        for (String endpointInfo : heroesEndpoints.split(",")) {
+            String[] parts = endpointInfo.split("=");
+            if (parts.length == 2 && parts[0].equals(endpoint)) {
+                return parts[1];
+            }
+        }
+        return "Unknown Hero";
     }
 }
 
